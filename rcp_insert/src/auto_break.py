13694caba324bc79d928ac033f0fa33c7b321b2d
S.Cendlak <stancendlak@gmail.com>
2021-12-17 11:30:05 +0100

    getting token by static function and added exception handling for further edit

--- rcp_insert/src/auto_break.py
+++ rcp_insert/src/auto_break.py
@@ -1,5 +1,6 @@
+import sys
+
 from bs4 import BeautifulSoup
-from datetime import datetime
 from fake_useragent import UserAgent
 import requests
 from src.objects import BasePayload, FirstPayload, SecondPayload
@@ -18,32 +19,55 @@ class BreakRcp:
     def set_user_agent(browser_type: str):
         ua = UserAgent()
         return {"User-Agent": ua[browser_type]}
+    @staticmethod
+    def get_verification_token(self, page):
+        try:
+            token = page.find('input', {'name': '__RequestVerificationToken'}).get('value')
+        except Exception as e:
+            print("Got unhandled exception %s" % str(e))
+        return token
 
     def add_break(self):
         with requests.Session() as session:
             session.auth = self.user_auth.auth_tuple()
             headers = self.set_user_agent(self.browser_type)
-            landing_page = session.get(URL1, verify=False, headers=headers)
-            soup = BeautifulSoup(session.get(URL1).content, 'html.parser')
             try:
-                token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
-            except Exception as e:
-                print("Got unhandled exception %s" % str(e))
+                landing_page = session.get(URL1, verify=False, headers=headers, timeout=5)
+            except requests.Timeout:
+                #possible infinite loop
+                self.add_break()
+            except requests.ConnectionError as err:
+                #add browser pop up
+                SystemExit(err)
+            page = BeautifulSoup(session.get(URL1).content, 'html.parser')
+            token = self.get_verification_token(page)
             self.user_auth.request_verification_token = str(token)
-            session.post(landing_page.url, data=self.user_auth.to_dict(), headers=headers)
+            session.post(landing_page.url, data=self.user_auth.to_dict(), headers=headers, timeout=5)
             first_payload = FirstPayload.set_instance(self.user_auth.__dict__)
-            second_page = session.post(URL2, data=first_payload.to_dict(), headers=headers)
+            try:
+                second_page = session.post(URL2, data=first_payload.to_dict(), headers=headers, timeout=5)
+            except requests.Timeout:
+                self.add_break()
+            except requests.ConnectionError as err:
+                #add browser pop up
+                SystemExit(err)
             statuses = second_page.json()['statuses']
             for d in statuses:
                 if d['Name'] == 'Przerwa':
                     second_payload = SecondPayload().set_instance({
                         'status_id': d['Id'],
                         'qr': second_page.json()['qrCode'],
-                        'is_end': second_page.json()['preferedStatusIsEnd'],
+                        'is_end': second_page.json()['preferedStatusIsEnd'],  #typo in Inewi ¯\_(ツ)_/¯
                         'time_local_unix': second_page.json()['timeLocalUnix'],
                         'time_utc_unix': second_page.json()['timeUtcUnix'],
                         'time_zone_id': second_page.json()['timeZoneId']
                     })
+            try:
+                session.post(URL3, data=second_payload.to_dict(), headers=headers)
+            except requests.Timeout:
+                self.add_break()
+            except requests.ConnectionError as err:
+                #add browser pop up
+                SystemExit(err)
 
-            session.post(URL3, data=second_payload.to_dict(), headers=headers)
 
